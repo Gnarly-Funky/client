fcdec2c3bc5ff28c5a27f53caaea5c25e89a9e07
Robert Driskell <driskell.ba@gmail.com>
2019-09-23 15:41:01 -0500

    theme provided

--- gf-client/src/App.js
+++ gf-client/src/App.js
@@ -1,9 +1,17 @@
 import React from "react";
 import "./App.css";
 import Wrapper from "./Wrapper";
+import { ThemeProvider } from "@material-ui/styles";
+import { createMuiTheme } from "@material-ui/core";
+import useTheme from './theme';
 
 function App() {
-    return <Wrapper />
+    const theme = createMuiTheme(useTheme);
+    return (
+        <ThemeProvider theme={theme}>
+            <Wrapper />
+        </ThemeProvider>
+    )
 }
 
 export default App;
---
+++ gf-client/src/theme.js
@@ -0,0 +1,5 @@
+const useTheme = {
+
+}
+
+export default useTheme
