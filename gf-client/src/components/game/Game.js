d248068eabf12efc602ff24c08f45746ceb68a94
Andrew Benedict <atbenedict@gmail.com>
2019-09-26 21:13:24 -0400

    changing player lists

--- gf-client/src/components/game/Game.js
+++ gf-client/src/components/game/Game.js
@@ -64,6 +64,7 @@ const Game = props => {
         "room_description": "",
         "other_players": [],
     });
+    const [roomPlayers, setRoomPlayers] = useState([])
 
     const [wPress, setWDisabled] = useKeyPress("w");
     const [aPress, setADisabled] = useKeyPress("a");
@@ -114,8 +115,19 @@ const Game = props => {
             .get('https://gnarly-funky.herokuapp.com/api/adv/init/', { headers: { authorization: headthing } })
             .then(response => {
                 console.log(response.data)
+                let myId = response.data.room_id
                 setServerPlayer(response.data)
                 setPlayer({ x: response.data.room_x, y: response.data.room_y })
+                axios
+                    .post('https://gnarly-funky.herokuapp.com/api/adv/move/', { 'room_id': myId }, { headers: { authorization: headthing } })
+                    .then(response => {
+                        setRoomPlayers(response.data.other_players)
+                        console.log(`other players in my room: ${ response.data.other_players }`)
+                    })
+                    .catch(error => {
+                        console.log(error);
+                    });
+
             })
             .catch(error => {
                 console.log(error);
@@ -177,7 +189,9 @@ const Game = props => {
         axios
             .post('https://gnarly-funky.herokuapp.com/api/adv/move/', { 'room_id': newId }, { headers: { authorization: headthing } })
             .then(response => {
-                console.log(response.data)
+                if (response.data.other_players) {
+                    setRoomPlayers(response.data.other_players)
+                }
             })
             .catch(error => {
                 console.log(error);
@@ -242,7 +256,7 @@ const Game = props => {
                             <div >
                                 <div className={classes.playerlistTitle}>Playerlist</div>
                                 <div>
-                                    {serverPlayer.other_players.map(player => {
+                                    {roomPlayers.map(player => {
                                         return(
                                         <div className={classes.playerlist}>{player.username}</div>
                                     )
@@ -285,10 +299,16 @@ const Game = props => {
                                 }
                                 onClick={inventoryTab}
                             >
-                                Inventory
+                                Players
                             </div>
                         </div>
-                        {currentTab === "chat" ? <Chat setFocus={setFocus} serverPlayer={serverPlayer}/> : <Inventory />}
+                        {currentTab === "chat" ? <Chat setFocus={setFocus} serverPlayer={serverPlayer}/> : <div>
+                                    {serverPlayer.other_players.map(player => {
+                                        return(
+                                        <div className={classes.playerlist}>{player.username}</div>
+                                    )
+                                    })}
+                                </div>}
                     </div>
                 </div>
             </div>
