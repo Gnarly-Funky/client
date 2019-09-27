d5eb897939e34fc984df24a8308c22ea8427613d
Robert Driskell <driskell.ba@gmail.com>
2019-09-26 19:57:15 -0500

    Finished styling I think

--- gf-client/src/components/game/GameStyles.js
+++ gf-client/src/components/game/GameStyles.js
@@ -137,7 +137,7 @@ const gameStyles = makeStyles(theme => ({
     },
     message: {
         marginLeft: "1rem",
-        marginRight: "1rem",
+        marginRight: "3rem",
         marginBottom: "1rem",
         display: "flex",
         width: "100%",
